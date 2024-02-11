a736f34ad41689f0b87743519ebd992a1ad29bfb
klperiwal <klperiwal@gmail.com>
2024-02-11 11:28:40 +0530

    Included gender ratio graph

--- placement.py
+++ placement.py
@@ -1,11 +1,16 @@
-import streamlit as st
+import numpy as np
 import pandas as pd
-import preprocessor
-import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+import streamlit as st
+import preprocessor
+import seaborn as sns
+import plotly.express as px
 import plotly.figure_factory as ff
 
+import warnings
+warnings.filterwarnings("ignore")
 
 df_microsoft= pd.read_csv('Microsoft.csv')
 df_oracle= pd.read_csv('Oracle.csv')
@@ -13,7 +18,7 @@ df_oracle= pd.read_csv('Oracle.csv')
 # df1= preprocessor.fun(df_microsoft)
 # df2= preprocessor.fun(df_oracle)
 
-st.sidebar.title("Placement Analysis")
+st.sidebar.title("PLACEMENT ANALYSIS")
 st.sidebar.image('CareerSync.png')
 
 user_menu= st.sidebar.radio(
@@ -21,29 +26,45 @@ user_menu= st.sidebar.radio(
     ('Overall Analysis','Branch-wise Analysis','Company-wise Analysis','Placed Student Analysis')
 )
 
-selected_year= st.sidebar.selectbox('Select Year', list(range(2019, 2025)))
-selected_branch = st.sidebar.selectbox('Select Branch', ['IT', 'CSE', 'ECE', 'EEE', 'ICE'])
+selected_year= st.sidebar.selectbox('Select Year', list(range(2019,2024)))
+selected_branch = st.sidebar.selectbox('Select Branch', ['All','IT', 'CSE', 'ECE', 'EEE'])
 
 
-if user_menu == 'Overall Analysis':
+if user_menu=='Overall Analysis':
     st.title("Overall Placement Analysis")
     
     # CGPA distribution
     st.subheader("CGPA Distribution")
-    fig_cgpa= px.histogram(df_microsoft, x='CGPA', nbins=20, title='CGPA Distribution', labels={'CGPA': 'CGPA Score', 'count': 'Frequency'})
+    fig_cgpa= px.histogram(df_microsoft, x='CGPA', nbins=20, title='Overall CGPA Scores of Students', labels={'CGPA': 'CGPA Score', 'count': 'Frequency'})
     fig_cgpa.update_traces(marker_color='royalblue', marker_line_color='black', marker_line_width=1.5)
     fig_cgpa.update_layout(showlegend=False)
     st.plotly_chart(fig_cgpa)
 
-    # Branches Focus
-    st.subheader("Branches Focus")
+    # Mean, Max, Min, Median, and Mode CGPA analysis
+    mean_cgpa= df_microsoft['CGPA'].mean()
+    max_cgpa= df_microsoft['CGPA'].max()
+    min_cgpa= df_microsoft['CGPA'].min()
+    median_cgpa= df_microsoft['CGPA'].median()
+    mode_cgpa= df_microsoft['CGPA'].mode()
+
+    st.subheader("CGPA Analysis")
+    st.write(f"Mean CGPA: **{mean_cgpa:.2f}**")
+    st.write(f"Max CGPA: **{max_cgpa:.2f}**")
+    st.write(f"Min CGPA: **{min_cgpa:.2f}**")
+    st.write(f"Median CGPA: **{median_cgpa:.2f}**")
+    st.write(f"Mode CGPA: **{mode_cgpa.values[0]:.2f}**")
+
+    st.write("---") 
+    st.write("")
+
+    # Branch Focus
+    st.subheader("Students per Branch")
     branch_counts= df_microsoft['Branch'].value_counts()
-    fig_branch= px.bar(branch_counts, x=branch_counts.index, y=branch_counts.values, labels={'x':'Branch', 'y':'Count'}, title='Branches Focus', color=branch_counts.values)
-    fig_branch.update_traces(marker_line_color='black', marker_line_width=1.5)
+    fig_branch= px.bar(branch_counts, x=branch_counts.index, y=branch_counts.values, labels={'x':'Branch','y':'Count'}, title='Branches Focus')
+    fig_branch.update_traces(marker_line_color='black', marker_line_width=1)
     fig_branch.update_layout(showlegend=False)
     st.plotly_chart(fig_branch)
 
-    # Placed distribution
     st.subheader("Placed Distribution")
 
     # Select Company
@@ -54,7 +75,7 @@ if user_menu == 'Overall Analysis':
         placed_counts= df_oracle['Placed'].value_counts()
 
     fig_placed= px.pie(placed_counts, values=placed_counts.values, names=placed_counts.index, title='Placed Distribution')
-    fig_placed.update_traces(marker=dict(colors=['green', 'red'], line=dict(color='#000000', width=2)))
+    fig_placed.update_traces(marker=dict(colors=['green','red'], line=dict(color='#000000',width=2)))
     st.plotly_chart(fig_placed)
 
     # Branch vs CGPA
@@ -71,11 +92,11 @@ if user_menu == 'Overall Analysis':
     fig_marks.update_layout(showlegend=True)
     st.plotly_chart(fig_marks)
 
-
-elif user_menu == 'Placed Student Analysis':
-    st.title("Placed Students Analysis [Company Wise]")
+elif user_menu=='Placed Student Analysis':
+    st.title("Placed Students Analysis")
+    st.title("[Company Wise]")
     
-    selected_company= st.selectbox('Select Company', ['Microsoft', 'Oracle'])
+    selected_company= st.selectbox('Select Company',['Microsoft','Oracle'])
     if selected_company=='Microsoft':
         df_placed= df_microsoft[df_microsoft['Placed']=='YES']
     elif selected_company=='Oracle':
@@ -85,85 +106,95 @@ elif user_menu == 'Placed Student Analysis':
     if selected_gender!= 'All':
         df_placed = df_placed[df_placed['Gender']==selected_gender]
 
-    # CGPA trends for specially placed students
+    # CGPA trends for placed students
     st.subheader("CGPA Trends for Placed Students")
-    fig_cgpa_placed= px.line(df_placed, x=df_placed.index, y='CGPA', title='CGPA Trends for Placed Students')
+    fig_cgpa_placed= px.line(df_placed, x=df_placed['Name'], y='CGPA', title='CGPA Trends for Placed Students')
+    # fig_cgpa_placed= px.line(df_placed, x=df_placed['Branch'], y='CGPA', title='CGPA Trends for Placed Students')
     fig_cgpa_placed.update_traces(marker_color='green', marker_line_color='black', marker_line_width=1.5)
     st.plotly_chart(fig_cgpa_placed)
 
-    # Branch distribution for specially placed students
+    # Branch distribution for placed students
     st.subheader("Branch Distribution for Placed Students")
-    branch_counts_placed = df_placed['Branch'].value_counts()
-    fig_branch_placed = px.bar(branch_counts_placed, x=branch_counts_placed.index, y=branch_counts_placed.values, labels={'x':'Branch', 'y':'Count'}, title='Branch Distribution for Specially Placed Students')
+    branch_counts_placed= df_placed['Branch'].value_counts()
+    fig_branch_placed= px.bar(branch_counts_placed, x=branch_counts_placed.index, y=branch_counts_placed.values, labels={'x':'Branch', 'y':'Count'}, title='Branch Distribution')
     fig_branch_placed.update_traces(marker_line_color='black', marker_line_width=1.5)
     st.plotly_chart(fig_branch_placed)
 
-    # 10th and 12th Marks for specially placed students
+    # 10th and 12th Marks for placed students
     st.subheader("10th and 12th Marks for Placed Students")
-    fig_marks_placed = px.scatter(df_placed, x='10th Percentage', y='12th Percentage', title='10th vs 12th Marks for Specially Placed Students', color='Branch')
+    fig_marks_placed= px.scatter(df_placed, x='10th Percentage', y='12th Percentage', title='10th vs 12th Marks for  Placed Students', color='Branch')
     fig_marks_placed.update_traces(marker_line_color='black', marker_line_width=1.5)
     fig_marks_placed.update_layout(showlegend=True)
     st.plotly_chart(fig_marks_placed)
 
 
-# Branch-wise Analysis
-elif user_menu =='Branch-wise Analysis':
+elif user_menu=='Branch-wise Analysis':
     st.title("Branch-wise Placement Analysis")
     
+    # Select Company
+    selected_company= st.selectbox('Select Company',['Microsoft','Oracle'])
+
+    if selected_company=='Microsoft':
+        company=df_microsoft
+    elif selected_company=='Oracle':
+        company=df_oracle
+
     # Select Branch
-    selected_branch = st.selectbox('Select Branch', df_microsoft['Branch'].unique())
+    selected_branch= st.selectbox('Select Branch', company['Branch'].unique())
 
     # CGPA distribution for selected branch
-    st.subheader(f"CGPA Distribution for {selected_branch}")
-    fig_cgpa_branch = px.histogram(df_microsoft[df_microsoft['Branch'] == selected_branch], x='CGPA', nbins=20, title=f'CGPA Distribution for {selected_branch}', labels={'CGPA': 'CGPA Score', 'count': 'Frequency'})
+    st.subheader(f"CGPA Distribution")
+    fig_cgpa_branch= px.histogram(company[company['Branch']==selected_branch], x='CGPA', nbins=20, title=f'CGPA Distribution of the students opted', labels={'CGPA': 'CGPA Score', 'count': 'Frequency'})
+    fig_cgpa_branch= px.histogram(company[company['Branch']==selected_branch], x='CGPA', nbins=20, title=f'CGPA Distribution of the students opted', labels={'CGPA': 'CGPA Score', 'count': 'Frequency'})
     fig_cgpa_branch.update_traces(marker_color='green', marker_line_color='black', marker_line_width=1.5)
     fig_cgpa_branch.update_layout(showlegend=False)
     st.plotly_chart(fig_cgpa_branch)
 
     # 10th and 12th Marks for selected branch
-    st.subheader(f"10th and 12th Marks for {selected_branch}")
-    fig_marks_branch = px.scatter(df_microsoft[df_microsoft['Branch'] == selected_branch], x='10th Percentage', y='12th Percentage', title=f'10th vs 12th Marks for {selected_branch}', color='CGPA')
+    st.subheader(f"10th and 12th Marks")
+    fig_marks_branch= px.scatter(company[company['Branch'] == selected_branch], x='10th Percentage', y='12th Percentage', title=f'10th vs 12th Marks for {selected_branch}', color='CGPA')
     fig_marks_branch.update_traces(marker_line_color='black', marker_line_width=1.5)
     fig_marks_branch.update_layout(showlegend=True)
     st.plotly_chart(fig_marks_branch)
 
-    # Gender ratio in each class (branch)
-    st.subheader(f"Gender Ratio in {selected_branch}")
-    ratio = df_microsoft[df_microsoft['Branch'] == selected_branch]['Gender'].value_counts()
-    fig_ratio = px.bar(ratio, x=ratio.index, y=ratio.values, title=f"Gender Ratio in {selected_branch}",color=['Boys','Girls'])
-    fig_ratio.update_layout(xaxis_title="Gender", yaxis_title="Count")
-    st.plotly_chart(fig_ratio)
+    # Gender ratio in selected branch
+    st.subheader(f"Gender Ratio")
+    gender_ratio = company[company['Branch'] == selected_branch]['Gender'].value_counts()
+    fig_gender_ratio = px.bar(x=gender_ratio.index, y=gender_ratio.values, title=f"Gender Ratio", color=gender_ratio.index, color_discrete_map={'Male': 'blue', 'Female': 'pink'})
+    fig_gender_ratio.update_layout(xaxis_title="Gender", yaxis_title="Count")
+    st.plotly_chart(fig_gender_ratio)
+
 
 
 elif user_menu=='Company-wise Analysis':
     st.title("Company-wise Placement Analysis")
     
     # Select Company
-    selected_company= st.selectbox('Select Company', ['Microsoft','Oracle'])
+    selected_company= st.selectbox('Select Company',['Microsoft','Oracle'])
 
     if selected_company=='Microsoft':
-        df_company= df_microsoft
+        company= df_microsoft
     else:
-        df_company= df_oracle
+        company= df_oracle
 
     # CGPA distribution for selected company
     st.subheader(f"CGPA Distribution for {selected_company}")
-    fig_cgpa_company= px.histogram(df_company, x='CGPA', nbins=20, title=f'CGPA Distribution for {selected_company}', labels={'CGPA': 'CGPA Score', 'count': 'Frequency'})
+    fig_cgpa_company= px.histogram(company, x='CGPA', nbins=20, title=f'CGPA Distribution for {selected_company}', labels={'CGPA': 'CGPA Score', 'count': 'Frequency'})
     fig_cgpa_company.update_traces(marker_color='red', marker_line_color='black', marker_line_width=1.5)
     fig_cgpa_company.update_layout(showlegend=False)
     st.plotly_chart(fig_cgpa_company)
 
     # Branches focus for selected company
-    st.subheader(f"Branches Focus for {selected_company}")
-    branch_counts_company= df_company['Branch'].value_counts()
-    fig_branch_company= px.bar(branch_counts_company, x=branch_counts_company.index, y=branch_counts_company.values, labels={'x':'Branch', 'y':'Count'}, title=f'Branches Focus for {selected_company}', color=branch_counts_company.index, color_continuous_scale='RdYlBu')
+    st.subheader(f"Students opted per branch for {selected_company}")
+    branch_counts_company= company['Branch'].value_counts()
+    fig_branch_company= px.bar(branch_counts_company, x=branch_counts_company.index, y=branch_counts_company.values, labels={'x':'Branch', 'y':'Count'}, title=f'Branches Focus for {selected_company}', color=branch_counts_company.index)
     fig_branch_company.update_traces(marker_line_color='black', marker_line_width=1.5)
     fig_branch_company.update_layout(showlegend=False)
     st.plotly_chart(fig_branch_company)
 
     # 10th and 12th Marks for selected company
-    st.subheader(f"10th and 12th Marks for {selected_company}")
-    fig_marks_company= px.scatter(df_company, x='10th Percentage', y='12th Percentage', title=f'10th vs 12th Marks for {selected_company}', color='Branch')
+    st.subheader(f"10th and 12th Marks of the students applying for {selected_company}")
+    fig_marks_company= px.scatter(company, x='10th Percentage', y='12th Percentage', title=f'10th vs 12th Marks for {selected_company}', color='Branch')
     fig_marks_company.update_traces(marker_line_color='black', marker_line_width=1.5)
     fig_marks_company.update_layout(showlegend=True)
     st.plotly_chart(fig_marks_company)
--- preprocessor.py
+++ preprocessor.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+import streamlit as st
 
 def fun(df):
     # filtering for eligible candidates with CGPA>=7
@@ -6,5 +8,8 @@ def fun(df):
 
     df.drop_duplicates(inplace=True)
     # one hot encoding Branches- CSE, IT, ECE, EE
+    df.drop_duplicates(inplace=True)
+    df= df[df['Branch'] in list(['CSE','IT','ECE','EE'])]
     df= pd.concat([df,pd.get_dummies(df['Branch'])],axis=1)
+
     return df
